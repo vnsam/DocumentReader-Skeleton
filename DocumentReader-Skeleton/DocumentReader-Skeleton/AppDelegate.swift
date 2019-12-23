b48d9d9971ef57793a47aa536758198bc990a585
Vignesh, Narayanasamy <vigneshnv7@gmail.com>
2019-12-23 17:05:18 +0100

    Initial commit.

---
+++ DocumentReader-Skeleton/DocumentReader-Skeleton/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DocumentReader-Skeleton
+//
+//  Created by Vignesh Narayanasamy on 2019-12-23.
+//  Copyright © 2019 DispatchQ. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
